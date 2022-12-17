c1e2cdc3c0f88c9b255051c34c79d8ac10e84773
raeesa99 <raeesapatel99@hotmail.com>
2022-12-17 00:39:23 +0000

    added css to movieInfo page

--- index.js
+++ index.js
@@ -18,7 +18,20 @@ app.get('/movies', async (req, res) => {
     let data = await fetch('https://api.themoviedb.org/3/movie/upcoming?api_key=caf170e550f9e4c80f6cd7464404b60b&language=en-GB');
     data = await data.json();
     // console.log(data);
+    let movies = data.results.map((movie) => {
+        return {
+            title: movie.title,
+            image: 'https://image.tmdb.org/t/p/w300' + movie.poster_path,
+        };
+    });
+        // console.log(movies);
+        res.render('movies', {movies});
+});
 
+app.get('/movies/:movieID', async (req, res) => {
+    let data = await fetch('https://api.themoviedb.org/3/movie/upcoming?api_key=caf170e550f9e4c80f6cd7464404b60b&language=en-GB');
+    data = await data.json();
+    
     let movies = data.results.map((movie) => {
         return {
             title: movie.title,
@@ -28,12 +41,6 @@ app.get('/movies', async (req, res) => {
             //similar movies??
         };
     });
-
-        // console.log(movies);
-        res.render('movies', {movies});
-});
-
-app.get('/movies/:movieID', async (req, res) => {
     res.render('movieinfo', movies[req.params.movieID]);
 });
 
